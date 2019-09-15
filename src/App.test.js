bd9c42e283230347ef7f8051d5e3ab5f4fe647eb
Pranjal Sharma <pr306500>
2019-09-15 23:50:37 +0530

    docker file added

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('dsfsadfa', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
