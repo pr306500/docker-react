acf48d64af322d7f4f01dd1cf0494206d0c325ac
Pranjal Sharma <pr306500>
2019-09-16 23:51:51 +0530

    travis file added

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,3 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-it('dsfsadfa', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
